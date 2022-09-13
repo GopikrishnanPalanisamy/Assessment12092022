0e1135848c59710f03621d457255244b760e25eb
GopikrishnanPalanisamy <110168578+GopikrishnanPalanisamy@users.noreply.github.com>
2022-09-13 15:40:33 +0530

    Add files via upload

---
+++ TC001_CreateIndividual.java
@@ -0,0 +1,38 @@
+package com.salesforce.testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.framework.testng.api.base.ProjectSpecificMethods;
+import com.salesforce.pages.LoginPage;
+
+public class TC001_CreateIndividual extends ProjectSpecificMethods {
+	
+	@BeforeTest
+	public void setValues() {
+		testcaseName = "Create Individual";
+		testDescription ="Create a individual from salesforce";
+		authors="Gopi";
+		category ="Smoke";
+		excelFileName="testsalesforce";
+	}
+	
+	@Test(dataProvider = "fetchData")
+	public void runLogin(String username, String password, String lName, String fName) {
+		new LoginPage().enterUsername(username).enterPassword(password).clickLogin().clickMenu()
+		.clickViewAll1()
+		.selectIndividual()
+		.clickNew()
+		.enterLastName(lName)
+		.clickSave()
+		.verifyLastName(lName)
+		.clickEditIndividual()
+		.clickSalutation()
+		.selectSalutation()
+		.enterFirstName(fName)
+		.clickSave()
+		.verifyFirstName(fName,lName);
+		
+	}
+
+}
